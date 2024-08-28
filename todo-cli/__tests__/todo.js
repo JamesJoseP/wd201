2c077ba6eb428415efde3698d30cd47b8513681b
James Jose <jamesjose107@gmail.com>
2024-08-28 18:27:43 +0530

    Add 3 more tests for todo.js file

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -25,4 +25,51 @@ describe("ToDoList Test Suite", () => {
     markAsComplete(0);
     expect(all[0].completed).toBe(true);
   });
+  // MY CODE BELOW
+
+  test("Should check overdue items", () => {
+    const overdueDate = new Date(new Date().setDate(new Date().getDate() - 1));
+    const overdueItemsCount = all.filter(
+      (item) => item.dueDate < new Date().toISOString().slice(0, 10)
+    ).length;
+    add({
+      title: "Test",
+      completed: false,
+      dueDate: overdueDate.toISOString().slice(0, 10),
+    });
+    expect(
+      all.filter((item) => item.dueDate < new Date().toISOString().slice(0, 10))
+        .length
+    ).toBe(overdueItemsCount + 1);
+  });
+
+  test("Should check dueToday items", () => {
+    const dueTodayCount = all.filter(
+      (item) => item.dueDate === new Date().toISOString().slice(0, 10)
+    ).length;
+    add({
+      title: "Learn coding",
+      completed: false,
+      dueDate: new Date().toISOString().slice(0, 10),
+    });
+    expect(
+      all.filter(
+        (item) => item.dueDate === new Date().toISOString().slice(0, 10)
+      ).length
+    ).toBe(dueTodayCount + 1);
+  });
+  test("Due later items", () => {
+    const dueLaterDate = new Date(new Date().setDate(new Date().getDate() + 1));
+    const dueLaterCount = all.filter(
+      (item) => item.dueDate > new Date().toISOString().slice(0, 10)
+    ).length;
+    add({
+      title: "Drink Tea",
+      completed: false,
+      dueDate: dueLaterDate.toISOString().slice(0, 10),
+    });
+    expect(
+      all.filter((item) => item.dueDate > new Date().toISOString().slice(0, 10))
+    ).toHaveLength(dueLaterCount + 1);
+  });
 });

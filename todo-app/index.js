46b9fbbde38b03ad64ec4f4c58d4017dea0cd21b
James Jose <jamesjose107@gmail.com>
2024-09-01 16:30:07 +0530

    Create index.js

---
+++ todo-app/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+// define a route for this express.js application
+app.get("/", (request, response) => {
+  response.send("hello world");
+});
+
+app.listen(3000, () => {
+  console.log("Started express server at port 3000.");
+});
